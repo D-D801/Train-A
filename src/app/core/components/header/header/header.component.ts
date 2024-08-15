6a500226fbd44f0df27c0fd7a6d6b3eb552c6773
YuliyaVoronovich <Y-1306@mail.ru>
2024-08-15 23:08:27 +0300

    refactor: change dependency injection and rename menu (DD-22)

--- src/app/core/components/header/header/header.component.ts
+++ src/app/core/components/header/header/header.component.ts
@@ -1,12 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { TuiHeader } from '@taiga-ui/layout';
 import { LogoComponent } from '../logo/logo.component';
-import { MenuComponent } from '../menu/menu.component';
+import { HeaderMenuComponent } from '../header-menu/header-menu.component';
 
 @Component({
   selector: 'dd-header',
   standalone: true,
-  imports: [LogoComponent, MenuComponent, TuiHeader],
+  imports: [LogoComponent, HeaderMenuComponent, TuiHeader],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
