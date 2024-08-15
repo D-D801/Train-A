ec74b85b2e9d0451c79636b558f6f2840a10e7dd
YuliyaVoronovich <Y-1306@mail.ru>
2024-08-15 19:37:47 +0300

    feat: refactor header's sizes (DD-22)

--- src/app/core/components/header/header/header.component.ts
+++ src/app/core/components/header/header/header.component.ts
@@ -1,11 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { TuiHeader } from '@taiga-ui/layout';
 import { LogoComponent } from '../logo/logo.component';
 import { MenuComponent } from '../menu/menu.component';
 
 @Component({
   selector: 'dd-header',
   standalone: true,
-  imports: [LogoComponent, MenuComponent],
+  imports: [LogoComponent, MenuComponent, TuiHeader],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
