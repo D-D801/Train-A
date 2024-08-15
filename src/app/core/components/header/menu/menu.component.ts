d48675b642ba981b1ff9c8aa9df2eb552b17573d
YuliyaVoronovich <Y-1306@mail.ru>
2024-08-15 13:01:45 +0300

    feat: add auth and local-storage services (DD-22)

--- src/app/core/components/header/menu/menu.component.ts
+++ src/app/core/components/header/menu/menu.component.ts
@@ -1,14 +1,25 @@
+import { NgIf } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { AuthService } from '@features/auth/services/auth.service';
 import { TuiButton } from '@taiga-ui/core';
 import { TuiNavigation } from '@taiga-ui/layout';
 
 @Component({
   selector: 'dd-menu',
   standalone: true,
-  imports: [TuiNavigation, TuiButton, RouterLink],
+  imports: [TuiNavigation, TuiButton, RouterLink, NgIf],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MenuComponent {}
+export class MenuComponent {
+  public isLoggedIn: boolean;
+
+  public isAdminIn: boolean;
+
+  constructor(private authService: AuthService) {
+    this.isLoggedIn = this.authService.isLoggedIn();
+    this.isAdminIn = this.authService.isAdmin();
+  }
+}
