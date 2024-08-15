4eeaf2ca0e8895dd9033aeb704c95f3e8b9d7bd7
YuliyaVoronovich <Y-1306@mail.ru>
2024-08-15 20:15:54 +0300

    refactor: delete methods (DD-22)

--- src/app/features/auth/services/auth-api.service.ts
+++ src/app/features/auth/services/auth-api.service.ts
@@ -1,32 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { switchMap } from 'rxjs';
-
-const BASE_API_URL = 'path';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthApiService {
   constructor(private httpClient: HttpClient) {}
-
-  signup(body: { email: string; password: string }) {
-    return this.httpClient
-      .post(`${BASE_API_URL}/api/signup`, {
-        body,
-      })
-      .pipe(switchMap(() => this.signin(body)));
-  }
-
-  signin(body: { email: string; password: string }) {
-    return this.httpClient.post(`${BASE_API_URL}/api/signin`, {
-      body,
-    });
-  }
-
-  profile(body: { email: string; password: string }) {
-    return this.httpClient.post(`${BASE_API_URL}/api/profile`, {
-      body,
-    });
-  }
 }
