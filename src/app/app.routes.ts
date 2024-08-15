dbcc7acce35f6e7b705b5ca9c92c56ef2b4ceeea
YuliyaVoronovich <Y-1306@mail.ru>
2024-08-15 11:48:26 +0300

    feat: add main router's links (DD-22)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,37 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('@features/TrainA/pages/home-page/home-page.component').then((m) => m.HomePageComponent),
+  },
+  {
+    path: 'registration',
+    loadComponent: () =>
+      import('@features/auth/pages/registration-page/registration-page.component').then(
+        (m) => m.RegistrationPageComponent
+      ),
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('@features/auth/pages/login-page/login-page.component').then((m) => m.LoginPageComponent),
+  },
+  {
+    path: 'profile',
+    loadComponent: () =>
+      import('@features/auth/pages/profile-page/profile-page.component').then((m) => m.ProfilePageComponent),
+  },
+  {
+    path: 'orders',
+    loadComponent: () =>
+      import('@features/TrainA/pages/orders-page/orders-page.component').then((m) => m.OrdersPageComponent),
+  },
+  {
+    path: 'admin',
+    loadComponent: () =>
+      import('@features/admin/pages/admin-page/admin-page.component').then((m) => m.AdminPageComponent),
+  },
+];
--- src/app/core/components/header/menu/menu.component.spec.ts
+++ src/app/core/components/header/menu/menu.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { provideRouter } from '@angular/router';
 import { MenuComponent } from './menu.component';
 
 describe('MenuComponent', () => {
@@ -9,6 +10,7 @@ describe('MenuComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [MenuComponent],
+      providers: [provideRouter([])],
     }).compileComponents();
 
     fixture = TestBed.createComponent(MenuComponent);
