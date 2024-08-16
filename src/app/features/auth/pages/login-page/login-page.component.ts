40f06f87e063181b45920af653d806e19b1dbb3d
Andrei Markouski <insawm@gmail.com>
2024-08-16 23:14:33 +0300

    feat: add login layout (DD-3)

--- src/app/features/auth/pages/login-page/login-page.component.ts
+++ src/app/features/auth/pages/login-page/login-page.component.ts
@@ -1,11 +1,31 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AsyncPipe } from '@angular/common';
+import { TuiButton, TuiError } from '@taiga-ui/core';
+import { TuiFieldErrorPipe } from '@taiga-ui/kit';
+import { TuiInputModule, TuiInputPasswordModule } from '@taiga-ui/legacy';
 
 @Component({
   selector: 'dd-login-page',
   standalone: true,
-  imports: [],
+  imports: [
+    TuiError,
+    ReactiveFormsModule,
+    TuiFieldErrorPipe,
+    AsyncPipe,
+    TuiButton,
+    TuiInputPasswordModule,
+    TuiInputModule,
+  ],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LoginPageComponent {}
+export class LoginPageComponent {
+  private fb = inject(FormBuilder);
+
+  form: FormGroup = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
+  });
+}
