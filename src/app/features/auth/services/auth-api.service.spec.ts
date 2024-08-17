b14b8d37d48bcb507f9649f6dc6ce44995eb854a
YuliyaVoronovich <Y-1306@mail.ru>
2024-08-17 10:37:50 +0300

    feat: add tests for autn services (DD-2)

--- src/app/features/auth/services/auth-api.service.spec.ts
+++ src/app/features/auth/services/auth-api.service.spec.ts
@@ -1,17 +1,44 @@
 import { TestBed } from '@angular/core/testing';
 
 import { provideHttpClient } from '@angular/common/http';
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
+import { mockTokenResponse, mockUser } from '@shared/constants/mock-user-data';
 import { AuthApiService } from './auth-api.service';
+import { UserRequest, UserResponse } from '../interfaces/auth.interface';
 
 describe('AuthApiService', () => {
   let service: AuthApiService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({ providers: [provideHttpClient()] });
+    TestBed.configureTestingModule({ providers: [provideHttpClient(), provideHttpClientTesting()] });
     service = TestBed.inject(AuthApiService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should call signup endpoint and then signin endpoint', () => {
+    const userRequest: UserRequest = mockUser;
+    const signupResponse: UserResponse = mockTokenResponse;
+    const signinResponse: UserResponse = mockTokenResponse;
+
+    service.signup(userRequest).subscribe((response) => {
+      expect(response).toEqual(signinResponse);
+    });
+
+    const signupRequest = httpMock.expectOne('/api/signup');
+    expect(signupRequest.request.method).toBe('POST');
+    signupRequest.flush(signupResponse);
+
+    const signinRequest = httpMock.expectOne('/api/signin');
+    expect(signinRequest.request.method).toBe('POST');
+    signinRequest.flush(signinResponse);
   });
 });
---
+++ src/app/shared/constants/mock-user-data.ts
@@ -0,0 +1,11 @@
+export const mockUser = {
+  email: 'test@example.com',
+  password: 'Password123!',
+  confirmPassword: 'Password123!',
+  name: 'John',
+  lastName: 'Doe',
+};
+
+export const mockTokenResponse = {
+  token: 'testToken',
+};
