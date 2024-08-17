36a67c93298bde655f0cf7f25ce2bd772f2e217f
Andrei Markouski <insawm@gmail.com>
2024-08-17 15:24:51 +0300

    refactor: cancel button (DD-3)

--- src/app/features/auth/pages/login-page/login-page.component.spec.ts
+++ src/app/features/auth/pages/login-page/login-page.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { provideRouter } from '@angular/router';
 import { LoginPageComponent } from './login-page.component';
 
 describe('LoginPageComponent', () => {
@@ -9,6 +10,7 @@ describe('LoginPageComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [LoginPageComponent],
+      providers: [provideRouter([])],
     }).compileComponents();
 
     fixture = TestBed.createComponent(LoginPageComponent);
