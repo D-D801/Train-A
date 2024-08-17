7fed7f29e0d1118bb8c9b5f0ba937667619bbfb5
YuliyaVoronovich <Y-1306@mail.ru>
2024-08-17 07:51:45 +0300

    feat: add message guard admin (DD-2)

--- src/app/features/auth/guards/admin.guard.ts
+++ src/app/features/auth/guards/admin.guard.ts
@@ -1,12 +1,23 @@
 import { inject } from '@angular/core';
 import { CanMatchFn, Router } from '@angular/router';
+import { TuiAlertService } from '@taiga-ui/core';
 import { AuthService } from '../services/auth.service';
 
 export const adminGuard: CanMatchFn = () => {
   const authService: AuthService = inject(AuthService);
 
+  const alerts = inject(TuiAlertService);
+
+  const message = 'You do not have admin rights';
+
   if (authService.isAdminIn()) {
     return true;
   }
-  return inject(Router).createUrlTree(['/login']);
+  alerts
+    .open(message, {
+      label: 'Error:',
+      appearance: 'error',
+    })
+    .subscribe();
+  return inject(Router).createUrlTree(['/home']);
 };
