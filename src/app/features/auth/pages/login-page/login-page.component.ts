62d594658ca64c0b80f5110147345688e1f0931a
Andrei Markouski <insawm@gmail.com>
2024-08-18 17:18:08 +0300

    feat: add deferred form validator (DD-3)

--- src/app/features/auth/pages/login-page/login-page.component.ts
+++ src/app/features/auth/pages/login-page/login-page.component.ts
@@ -1,14 +1,13 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AsyncPipe } from '@angular/common';
+import { emailValidator, passwordValidator } from '@features/auth/validators';
 import { TuiButton, TuiError } from '@taiga-ui/core';
 import { TuiFieldErrorPipe } from '@taiga-ui/kit';
 import { TuiInputModule, TuiInputPasswordModule } from '@taiga-ui/legacy';
 import { RouterLink } from '@angular/router';
 import { AuthService } from '@features/auth/services/auth.service';
 import { TuiValidator } from '@taiga-ui/cdk';
-import { emailValidator } from '@features/auth/validators/password.validator';
-import { passwordValidator } from '@features/auth/validators/email.validator';
 
 @Component({
   selector: 'dd-login-page',
@@ -29,17 +28,32 @@ import { passwordValidator } from '@features/auth/validators/email.validator';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LoginPageComponent {
+  private isSubmitted = false;
+
   private fb = inject(FormBuilder);
 
   private authService = inject(AuthService);
 
   form: FormGroup = this.fb.group({
-    email: ['', [Validators.required, emailValidator()]],
-    password: ['', [Validators.required, passwordValidator()]],
+    email: [''],
+    password: [''],
   });
 
   protected onSubmit() {
+    this.isSubmitted = true;
+    this.form.get('email')?.setValidators([Validators.required, emailValidator()]);
+    this.form.get('email')?.updateValueAndValidity();
+    this.form.get('password')?.setValidators([Validators.required, passwordValidator()]);
+    this.form.get('password')?.updateValueAndValidity();
+    this.form.markAllAsTouched();
     const body = this.form.value;
     this.authService.signin(body);
+  }
+
+  protected checkSubmitStatus() {
+    if (this.isSubmitted) {
+      return this.form.invalid;
+    }
+    return this.form.pristine;
   }
 }
