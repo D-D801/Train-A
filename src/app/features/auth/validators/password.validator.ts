804d81fa20cdb9536f2986aa3b0b86475540cc7f
Andrei Markouski <insawm@gmail.com>
2024-08-18 22:14:58 +0300

    refactor: add custom errors (DD-3)

--- src/app/features/auth/validators/password.validator.ts
+++ src/app/features/auth/validators/password.validator.ts
@@ -1,6 +1,6 @@
 import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 
-export function passwordValidator(): ValidatorFn {
+export function passwordValidator(length: number): ValidatorFn {
   return (control: AbstractControl): ValidationErrors | null =>
-    !/.{8,}/.test(control.value.trim()) ? { minLength: 'At least 8 characters required' } : null;
+    control.value.trim().length < length ? { password: true } : null;
 }
---
+++ src/app/shared/constants/build-in-errors.constant.ts
@@ -0,0 +1,5 @@
+export const builtInErrors = {
+  required: 'This field is required',
+  emailRegex: 'Incorrect email',
+  password: 'At least 8 characters required',
+} as const;
