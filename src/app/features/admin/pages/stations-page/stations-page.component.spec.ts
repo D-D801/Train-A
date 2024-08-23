96a541c15eb27dbeb61a7c41d508c88f7e335e66
KarinaI <7karina.ibr@gmail.com>
2024-08-23 17:12:51 +0300

    feat: add stations-page (DD-13)

---
+++ src/app/features/admin/pages/stations-page/stations-page.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { StationsPageComponent } from './stations-page.component';
+
+describe('StationsPageComponent', () => {
+  let component: StationsPageComponent;
+  let fixture: ComponentFixture<StationsPageComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [StationsPageComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(StationsPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
