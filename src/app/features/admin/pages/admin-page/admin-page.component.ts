a71cb2286cc66df44928d860ff7f0dbb9b4ad47d
YuliyaVoronovich <Y-1306@mail.ru>
2024-08-26 12:17:17 +0300

    feat: add tabs to main admin page (DD-37)

--- src/app/features/admin/pages/admin-page/admin-page.component.ts
+++ src/app/features/admin/pages/admin-page/admin-page.component.ts
@@ -1,12 +1,17 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { TuiTab, TuiTabs } from '@taiga-ui/kit';
+import { TuiScrollbar } from '@taiga-ui/core';
+import { NgClass } from '@angular/common';
+import { CarriagePageComponent } from '../carriage-page/carriage-page.component';
 
 @Component({
   selector: 'dd-admin-page',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [TuiTabs, TuiTab, CarriagePageComponent, TuiScrollbar, NgClass],
   templateUrl: './admin-page.component.html',
   styleUrl: './admin-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AdminPageComponent {}
+export class AdminPageComponent {
+  protected activeItemIndex = 0;
+}
