d9fc9254268c791e35550a0d9a36d0ca28e25045
Andrei Markouski <insawm@gmail.com>
2024-08-27 18:10:05 +0300

    feat: add update, create form methods (DD-17)

--- src/app/features/admin/pages/tabs/route-tab/route-tab.component.ts
+++ src/app/features/admin/pages/tabs/route-tab/route-tab.component.ts
@@ -4,13 +4,13 @@ import { RouteCardComponent } from '@features/admin/components/route-card/route-
 import { RouteFormComponent } from '@features/admin/components/route-form/route-form/route-form.component';
 import { TrainRoute } from '@features/admin/interfaces/train-route.interface';
 import { RouteApiService } from '@features/admin/services/route-api/route-api.service';
-import { TuiSurface } from '@taiga-ui/core';
+import { TuiButton, TuiSurface } from '@taiga-ui/core';
 import { TuiCardLarge } from '@taiga-ui/layout';
 
 @Component({
   selector: 'dd-route-tab',
   standalone: true,
-  imports: [RouteFormComponent, RouteCardComponent, TuiCardLarge, TuiSurface],
+  imports: [RouteFormComponent, RouteCardComponent, TuiCardLarge, TuiSurface, TuiButton],
   templateUrl: './route-tab.component.html',
   styleUrl: './route-tab.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -22,15 +22,36 @@ export class RouteTabComponent {
 
   public readonly routes = signal<TrainRoute[]>([]);
 
+  public readonly currentRouteId = signal<number | null>(null);
+
+  public readonly isEdit = signal(false);
+
   public constructor() {
-    this.getRoutes()
+    this.getRoutes();
+  }
+
+  public getRoutes() {
+    return this.routeService
+      .getRoute()
       .pipe(takeUntilDestroyed(this.destroy))
       .subscribe((trainRoutes) => {
         this.routes.set(trainRoutes.slice(0, 3));
       });
   }
 
-  public getRoutes() {
-    return this.routeService.getRoute();
+  public onEdit(id: number | undefined) {
+    if (!id) return;
+    this.currentRouteId.set(id - 1);
+    this.isEdit.set(true);
+  }
+
+  public onSave() {
+    this.isEdit.set(false);
+    this.getRoutes();
+  }
+
+  public onCreate() {
+    this.currentRouteId.set(null);
+    this.isEdit.set(true);
   }
 }
