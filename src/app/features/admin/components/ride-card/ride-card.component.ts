bcd653756a879d36982e728f4b37c76e4e7cb065
Mariia Rykova <maryrykova24@gmail.com>
2024-08-27 17:11:09 +0400

    feat: add accordion for ride-card (DD-19)

--- src/app/features/admin/components/ride-card/ride-card.component.ts
+++ src/app/features/admin/components/ride-card/ride-card.component.ts
@@ -2,12 +2,13 @@ import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { TuiSurface, TuiTitle } from '@taiga-ui/core';
 import { TuiCardLarge, TuiHeader } from '@taiga-ui/layout';
 import { Schedule } from '@features/admin/interfaces/schedule.interface';
+import { TuiAccordion } from '@taiga-ui/kit';
 import { StationCardComponent } from '../station-card/station-card.component';
 
 @Component({
   selector: 'dd-ride-card',
   standalone: true,
-  imports: [TuiCardLarge, TuiSurface, TuiTitle, TuiHeader, StationCardComponent],
+  imports: [TuiCardLarge, TuiSurface, TuiTitle, TuiHeader, StationCardComponent, TuiAccordion],
   templateUrl: './ride-card.component.html',
   styleUrl: './ride-card.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
