b1080526fad6dc520ccd50bb5a4f825058e94d8b
Mariia Rykova <maryrykova24@gmail.com>
2024-08-31 18:41:00 +0400

    refactor: refactor update rides (DD-19)

--- src/app/features/admin/interfaces/route.interface.ts
+++
@@ -1,8 +0,0 @@
-import { Schedule } from './schedule.interface';
-
-export interface Route {
-  id: number;
-  path: number[];
-  carriages: string[];
-  schedule: Schedule[];
-}
--- src/app/features/admin/pages/schedule-page/schedule-page.component.ts
+++ src/app/features/admin/pages/schedule-page/schedule-page.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, DestroyRef, inject, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, DestroyRef, inject, signal, OnInit } from '@angular/core';
 import { RouteApiService } from '@features/admin/services/route-api/route-api.service';
 import { TuiButton } from '@taiga-ui/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
@@ -6,9 +6,9 @@ import { AlertService } from '@core/services/alert/alert.service';
 import { AsyncPipe, Location, NgFor } from '@angular/common';
 
 import { NewRideService } from '@features/admin/services/new-ride/new-ride.service';
-import { switchMap, tap } from 'rxjs';
-import { Route } from '@features/admin/interfaces/route.interface';
+import { merge, Subject, switchMap, tap } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
+import { TrainRoute } from '@features/admin/interfaces/train-route.interface';
 import { RideCardComponent } from '../../components/ride-card/ride-card.component';
 import { NewRideFormComponent } from '../../components/new-ride-form/new-ride-form.component';
 
@@ -20,7 +20,7 @@ import { NewRideFormComponent } from '../../components/new-ride-form/new-ride-fo
   styleUrl: './schedule-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SchedulePageComponent {
+export class SchedulePageComponent implements OnInit {
   private readonly routeApiService = inject(RouteApiService);
 
   protected readonly newRideService = inject(NewRideService);
@@ -33,19 +33,30 @@ export class SchedulePageComponent {
 
   public readonly destroy = inject(DestroyRef);
 
-  protected routeInformation = signal<Route>({} as Route);
+  protected routeInformation = signal<TrainRoute>({} as TrainRoute);
 
   protected carriages = signal<string[]>([]);
 
-  protected routeInformation$ = this.route.paramMap.pipe(
-    switchMap((params) => this.routeApiService.getRoute(Number(params.get('id')))),
-    tap({
-      next: (route) => {
+  private readonly routeUpdateSubject = new Subject<number>();
+
+  protected routeInformation$ = merge(
+    this.route.paramMap.pipe(
+      switchMap((params) => this.routeApiService.getRoute(Number(params.get('id')))),
+      tap((route) => {
         const currentCarriages = new Set<string>(route.carriages);
         this.carriages.set([...currentCarriages]);
 
         this.routeInformation.set(route);
-      },
+      })
+    ),
+    this.routeUpdateSubject.pipe(
+      switchMap((routeId) => this.routeApiService.getRoute(routeId)),
+      tap((route) => {
+        this.routeInformation.set(route);
+      })
+    )
+  ).pipe(
+    tap({
       error: ({ error: { message } }) => {
         this.alert.open({ message, label: 'Error', appearance: 'error' });
       },
@@ -53,12 +64,12 @@ export class SchedulePageComponent {
     takeUntilDestroyed()
   );
 
-  public constructor() {
+  public ngOnInit() {
     this.routeInformation$.subscribe();
   }
 
-  protected updateRouteInfo() {
-    this.routeInformation$.subscribe();
+  protected updateRouteInfo(routeId: number) {
+    this.routeUpdateSubject.next(routeId);
   }
 
   protected navigationBack() {
