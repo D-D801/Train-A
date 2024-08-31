2262881710d914e96be358a19637b676a8611c43
KarinaI <7karina.ibr@gmail.com>
2024-08-31 15:04:51 +0300

    fix: fix tests (DD-13)

--- src/app/features/admin/services/stations-api/stations-api.service.spec.ts
+++ src/app/features/admin/services/stations-api/stations-api.service.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { StationsApiService } from './stations-api.service';
 
 describe('StationsApiService', () => {
   let service: StationsApiService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({ providers: [provideHttpClient(), provideHttpClientTesting()] });
     service = TestBed.inject(StationsApiService);
   });
 
