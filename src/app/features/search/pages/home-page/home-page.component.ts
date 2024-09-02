699b84016bbdfbfdbfd358e38b109332cd060c08
KarinaI <7karina.ibr@gmail.com>
2024-09-02 19:27:54 +0300

    feat: add No rides available block (DD-29)

--- src/app/features/search/pages/home-page/home-page.component.ts
+++ src/app/features/search/pages/home-page/home-page.component.ts
@@ -1,12 +1,13 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject } from '@angular/core';
 import { SearchService } from '@features/search/services/search/search.service';
+import { TuiBlockStatus } from '@taiga-ui/layout';
 import { SearchFormComponent } from '../../components/search-form/search-form.component';
 import { SearchResultListComponent } from '../../components/search-result-list/search-result-list.component';
 
 @Component({
   selector: 'dd-home-page',
   standalone: true,
-  imports: [SearchFormComponent, SearchResultListComponent],
+  imports: [SearchFormComponent, SearchResultListComponent, TuiBlockStatus],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -15,4 +16,6 @@ export class HomePageComponent {
   private readonly searchService = inject(SearchService);
 
   public result = this.searchService.searchResult;
+
+  public hasResults = computed(() => Array.isArray(this.result()?.routes) && !!this.result()?.routes.length);
 }
