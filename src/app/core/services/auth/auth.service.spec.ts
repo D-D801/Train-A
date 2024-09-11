043fb198adc47dacf246eafc73fdd1b490a29e1a
Mariia Rykova <maryrykova24@gmail.com>
2024-09-11 17:37:16 +0400

    refactor: delete comments (DD-63)

--- src/app/core/services/auth/auth.service.spec.ts
+++ src/app/core/services/auth/auth.service.spec.ts
@@ -55,7 +55,7 @@ describe('AuthServiceService', () => {
 
     service.signup(mockUser).subscribe(() => {
       expect(localStorageServiceMock.setItem).toHaveBeenCalledWith(LocalStorageKey.UserToken, mockTokenResponse.token);
-      expect(localStorageServiceMock.setItem).toHaveBeenCalledWith(LocalStorageKey.UserRole, 'admin'); // если роль администратора
+      expect(localStorageServiceMock.setItem).toHaveBeenCalledWith(LocalStorageKey.UserRole, 'admin');
       expect(service.isLoggedIn()).toBeTruthy();
     });
   });
